11926919de8f2a4114de9c4d6a228d52a4bcd2e5
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 21:14:04 +0800

    solo-19

---
+++ src/components/FigmaComponent/index.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.frame32}>
+      <div className={styles.instance}>
+        <div className={styles.frame2090053469}>
+          <div className={styles.frame} />
+        </div>
+      </div>
+      <div className={styles.frame30}>
+        <div className={styles.frame2036096447}>
+          <div className={styles.frame7}>
+            <div className={styles.frame6}>
+              <div className={styles.ellipse5}>
+                <div className={styles.ellipse6}>
+                  <div className={styles.frame5}>
+                    <div className={styles.frame2} />
+                    <div className={styles.frame3} />
+                    <div className={styles.frame4} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame10}>
+            <div className={styles.frame8}>
+              <p className={styles.text}>第</p>
+              <p className={styles.a50}>50+</p>
+              <p className={styles.text}>名</p>
+            </div>
+            <div className={styles.frame9}>
+              <p className={styles.text2}>距上一名差</p>
+              <p className={styles.text3}>9990.99万</p>
+            </div>
+          </div>
+          <div className={styles.frame14}>
+            <div className={styles.frame12}>
+              <p className={styles.a}>+</p>
+              <p className={styles.a32}>32%</p>
+              <div className={styles.frame2090053472}>
+                <p className={styles.text4}>加成中</p>
+                <img
+                  src={require('../../assets/images/mevf5i5t-ciky84m.svg').default}
+                  className={styles.frame11}
+                />
+              </div>
+            </div>
+            <div className={styles.frame13}>
+              <p className={styles.text2}>额外获得</p>
+              <p className={styles.text3}>9999.99万盛典值</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.instance4}>
+          <div className={styles.instance2}>
+            <div className={styles.frame16}>
+              <p className={styles.text5}>巅峰1V1对决</p>
+              <div className={styles.frame15}>
+                <p className={styles.a0520}>05:20</p>
+              </div>
+            </div>
+            <div className={styles.frame17}>
+              <p className={styles.text2}>还剩x次加成获取机会</p>
+              <img src={require('../../assets/images/mevf5i5t-ciky84m.svg').default} className={styles.frame11} />
+            </div>
+          </div>
+          <div className={styles.instance3}>
+            <div className={styles.frame28}>
+              <div className={styles.frame18}>
+                <div className={styles.frame6}>
+                  <div className={styles.ellipse5}>
+                    <div className={styles.ellipse6}>
+                      <div className={styles.frame5}>
+                        <div className={styles.frame2} />
+                        <div className={styles.frame3} />
+                        <div className={styles.frame4} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.frame25}>
+                <div className={styles.frame19}>
+                  <p className={styles.text6}>我方昵称</p>
+                  <p className={styles.text7}>对方的昵称...</p>
+                </div>
+                <div className={styles.frame24}>
+                  <div className={styles.frame23}>
+                    <div className={styles.frame20} />
+                    <div className={styles.frame22}>
+                      <div className={styles.frame21}>
+                        <p className={styles.text8}>9999</p>
+                        <p className={styles.text9}>9999</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.frame27}>
+                <div className={styles.frame26}>
+                  <div className={styles.ellipse5}>
+                    <div className={styles.ellipse6}>
+                      <div className={styles.frame5}>
+                        <div className={styles.frame2} />
+                        <div className={styles.frame3} />
+                        <div className={styles.frame4} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.frame29}>
+              <p className={styles.text10}>胜方将获得5%加成，败方扣除3%榜值</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src={require('../../assets/images/mevf5i5t-gdms6i7.svg').default} className={styles.frame31} />
+    </div>
+  );
+}
+
+export default Component;
