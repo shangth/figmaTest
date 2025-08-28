8132f9c1292ff6c22db764b21d112b668d9e3ad4
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 20:24:42 +0800

    solo-16

---
+++ src/components/FigmaComponent/index.jsx
@@ -0,0 +1,343 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.frame26}>
+      <div className={styles.barsStatusBarsIPhone}>
+        <p className={styles.time}>9:41</p>
+        <img src="../../image/mevddxl5-ontyyvj.svg" className={styles.frame} />
+      </div>
+      <div className={styles.background}>
+        <img src="../../image/mevddxl5-eg3i3ut.svg" className={styles.frame2} />
+        <div className={styles.searchbarInput}>
+          <p className={styles.text}>一点点</p>
+          <img src="../../image/mevddxl5-qzae4ua.svg" className={styles.frame3} />
+        </div>
+        <p className={styles.text2}>搜索</p>
+      </div>
+      <div className={styles.frame5}>
+        <div className={styles.autoWrapper}>
+          <p className={styles.text3}>综合</p>
+          <p className={styles.text3}>智能</p>
+          <div className={styles.frame4}>
+            <p className={styles.text4}>团购</p>
+            <div className={styles.tabLine} />
+          </div>
+          <p className={styles.text3}>视频</p>
+          <p className={styles.text3}>用户</p>
+          <p className={styles.text3}>商品</p>
+          <p className={styles.text3}>直播</p>
+        </div>
+        <div className={styles.line1} />
+      </div>
+      <div className={styles.frame2053140790}>
+        <div className={styles.group2090053420}>
+          <div className={styles.instance}>
+            <div className={styles.healthHomepageExpert} />
+            <div className={styles.autoWrapper2}>
+              <p className={styles.text5}>一点点（辉煌国际店）</p>
+              <div className={styles.frame919}>
+                <div className={styles.frame6}>
+                  <div className={styles.rectangle11114129}>
+                    <img
+                      src="../../image/mevddxl6-h4euvef.svg"
+                      className={styles.topIcon}
+                    />
+                  </div>
+                  <p className={styles.text6}>北京饮品畅销榜 TOP2</p>
+                </div>
+                <div className={styles.frame7}>
+                  <p className={styles.text7}>50人看过</p>
+                </div>
+              </div>
+              <div className={styles.frame920}>
+                <p className={styles.text8}>饮品</p>
+                <p className={styles.text8}>海淀区</p>
+                <p className={styles.text8}>¥17/人</p>
+                <p className={styles.text9}>海淀区</p>
+              </div>
+            </div>
+            <p className={styles.a39Km}>3.9km</p>
+          </div>
+          <div className={styles.frame15}>
+            <div className={styles.component17}>
+              <div className={styles.frame8}>
+                <div className={styles.rectangle111142006}>
+                  <p className={styles.text10}>快手更低价</p>
+                </div>
+                <div className={styles.bottomMask}>
+                  <p className={styles.text11}>已抢259</p>
+                </div>
+              </div>
+              <div className={styles.frame9}>
+                <div className={styles.autoWrapper3}>
+                  <div className={styles.rectangle780}>
+                    <p className={styles.text12}>新品</p>
+                  </div>
+                  <p className={styles.text13}>草莓啵啵奶茶</p>
+                </div>
+                <p className={styles.text14}>超多料大杯分享</p>
+                <div className={styles.autoWrapper4}>
+                  <p className={styles.a}>¥</p>
+                  <p className={styles.a19}>19</p>
+                  <div className={styles.group1312320643}>
+                    <p className={styles.text15}>¥22</p>
+                    <div className={styles.line3} />
+                  </div>
+                  <div className={styles.frame1312320623}>
+                    <p className={styles.text16}>6.7折</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.component172}>
+              <div className={styles.frame10}>
+                <div className={styles.rectangle7802}>
+                  <p className={styles.text17}>美团补贴</p>
+                </div>
+                <div className={styles.bottomMask}>
+                  <p className={styles.text11}>已抢259</p>
+                </div>
+              </div>
+              <div className={styles.frame11}>
+                <div className={styles.autoWrapper5}>
+                  <div className={styles.rectangle7803}>
+                    <p className={styles.text12}>爆款</p>
+                  </div>
+                  <p className={styles.text14}>周末专享乌龙</p>
+                </div>
+                <p className={styles.text14}>茶饮不含咖啡因</p>
+                <div className={styles.autoWrapper6}>
+                  <div className={styles.group179}>
+                    <p className={styles.a}>¥</p>
+                    <p className={styles.a19}>14.9</p>
+                  </div>
+                  <div className={styles.group13123206432}>
+                    <p className={styles.text15}>¥29</p>
+                    <div className={styles.line3} />
+                  </div>
+                  <div className={styles.frame13123206232}>
+                    <p className={styles.text18}>4.3折</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.component25}>
+              <div className={styles.frame12}>
+                <div className={styles.rectangle7802}>
+                  <p className={styles.text17}>美团补贴</p>
+                </div>
+                <div className={styles.bottomMask}>
+                  <p className={styles.text11}>已抢259</p>
+                </div>
+              </div>
+              <div className={styles.frame13}>
+                <p className={styles.text14}>杨枝甘露双倍芒果</p>
+                <p className={styles.text14}>西柚粒冰沙</p>
+                <div className={styles.autoWrapper7}>
+                  <div className={styles.group1792}>
+                    <p className={styles.a}>¥</p>
+                    <p className={styles.a19}>18.9</p>
+                  </div>
+                  <div className={styles.group1312320643}>
+                    <p className={styles.text15}>¥29</p>
+                    <div className={styles.line3} />
+                  </div>
+                  <div className={styles.frame13123206233}>
+                    <p className={styles.text18}>4.5折</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.component12}>
+              <div className={styles.image}>
+                <div className={styles.bottomMask2}>
+                  <p className={styles.text11}>已售259</p>
+                </div>
+              </div>
+              <div className={styles.group926}>
+                <div className={styles.frame14}>
+                  <p className={styles.text19}>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;老友记拔草双人套餐
+                  </p>
+                  <p className={styles.a2}>¥</p>
+                </div>
+                <div className={styles.group1312320645}>
+                  <p className={styles.text20}>快手补贴</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.autoWrapper9}>
+        <div className={styles.frame18}>
+          <div className={styles.healthHomepageExpert} />
+          <div className={styles.autoWrapper8}>
+            <p className={styles.text5}>一点点（辉煌国际店）</p>
+            <div className={styles.frame2036092082}>
+              <div className={styles.frame16}>
+                <p className={styles.text21}>购买过</p>
+              </div>
+              <div className={styles.frame17}>
+                <p className={styles.text22}>消费人数2K+</p>
+              </div>
+              <div className={styles.frame17}>
+                <p className={styles.text22}>“太好喝啦”</p>
+              </div>
+            </div>
+            <div className={styles.frame920}>
+              <p className={styles.text8}>饮品</p>
+              <p className={styles.text8}>海淀区</p>
+              <p className={styles.text8}>¥17/人</p>
+              <p className={styles.text9}>海淀区</p>
+            </div>
+          </div>
+          <p className={styles.a39Km}>3.9km</p>
+        </div>
+        <div className={styles.frame2147224468}>
+          <p className={styles.text23}>已折叠12个较远商家</p>
+          <img src="../../image/mevddxl6-fsv8g5r.svg" className={styles.frame19} />
+        </div>
+      </div>
+      <div className={styles.frame2053140792}>
+        <div className={styles.group2090053415}>
+          <p className={styles.text24}>附近优惠推荐</p>
+          <div className={styles.frame2053140797}>
+            <div className={styles.group2090053439}>
+              <div className={styles.instance2}>
+                <div className={styles.image2} />
+                <div className={styles.autoWrapper10}>
+                  <p className={styles.text5}>沪上阿姨（西二旗店）</p>
+                  <div className={styles.frame20}>
+                    <p className={styles.text7}>10人看过</p>
+                  </div>
+                  <div className={styles.frame9202}>
+                    <p className={styles.text8}>饮品</p>
+                    <p className={styles.text8}>海淀区</p>
+                  </div>
+                </div>
+                <p className={styles.a39Km}>3.3km</p>
+              </div>
+              <div className={styles.frame23}>
+                <div className={styles.image34}>
+                  <div className={styles.frame21}>
+                    <img
+                      src="../../image/mevddxl5-w1xs98j.png"
+                      className={styles.localLifeLocallifeTu}
+                    />
+                    <p className={styles.text25}>直播团购</p>
+                  </div>
+                  <div className={styles.rectangle3658}>
+                    <p className={styles.text26}>二姐带你逛吃直播</p>
+                  </div>
+                </div>
+                <div className={styles.component24}>
+                  <div className={styles.image3}>
+                    <div className={styles.bottomMask2}>
+                      <p className={styles.text11}>已抢259</p>
+                    </div>
+                  </div>
+                  <div className={styles.frame13}>
+                    <p className={styles.text14}>杨枝甘露双倍芒果</p>
+                    <p className={styles.text14}>西柚粒冰沙</p>
+                    <div className={styles.autoWrapper7}>
+                      <div className={styles.group1792}>
+                        <p className={styles.a}>¥</p>
+                        <p className={styles.a19}>18.9</p>
+                      </div>
+                      <div className={styles.group1312320643}>
+                        <p className={styles.text15}>¥29</p>
+                        <div className={styles.line3} />
+                      </div>
+                      <div className={styles.frame13123206233}>
+                        <p className={styles.text18}>4.5折</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.component242}>
+                  <div className={styles.image4}>
+                    <div className={styles.bottomMask2}>
+                      <p className={styles.text11}>已抢259</p>
+                    </div>
+                  </div>
+                  <div className={styles.frame13}>
+                    <p className={styles.text14}>珍珠奶茶波波超大</p>
+                    <p className={styles.text14}>杯美味好喝</p>
+                    <div className={styles.autoWrapper7}>
+                      <div className={styles.group1792}>
+                        <p className={styles.a}>¥</p>
+                        <p className={styles.a19}>18.9</p>
+                      </div>
+                      <div className={styles.group1312320643}>
+                        <p className={styles.text15}>¥29</p>
+                        <div className={styles.line3} />
+                      </div>
+                      <div className={styles.frame13123206233}>
+                        <p className={styles.text18}>4.5折</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.group215}>
+                  <div className={styles.frame22}>
+                    <div className={styles.bottomMask2}>
+                      <p className={styles.text11}>已售259</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.group2090053438}>
+              <div className={styles.instance3}>
+                <div className={styles.image2} />
+                <div className={styles.autoWrapper11}>
+                  <p className={styles.text5}>沪上阿姨（上地店）</p>
+                  <div className={styles.frame24}>
+                    <p className={styles.text7}>暂无评价</p>
+                  </div>
+                  <div className={styles.frame9202}>
+                    <p className={styles.text8}>饮品</p>
+                    <p className={styles.text8}>海淀区</p>
+                  </div>
+                </div>
+                <p className={styles.a39Km}>3.3km</p>
+              </div>
+              <div className={styles.frame25}>
+                <div className={styles.group2152}>
+                  <img
+                    src="../../image/mevddxl5-umsn73u.png"
+                    className={styles.image5}
+                  />
+                </div>
+                <div className={styles.group2153}>
+                  <img
+                    src="../../image/mevddxl5-6f42cco.png"
+                    className={styles.image6}
+                  />
+                </div>
+                <div className={styles.group2153}>
+                  <img
+                    src="../../image/mevddxl5-lg0ajmg.png"
+                    className={styles.image6}
+                  />
+                </div>
+                <div className={styles.group2154}>
+                  <img
+                    src="../../image/mevddxl5-4a4pixn.png"
+                    className={styles.image5}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
