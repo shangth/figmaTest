88ac3b3e0b2f524c849a30e50468a57fdea8d4a2
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 14:22:34 +0800

    solo-10

---
+++ src/FigmaComponent.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+import styles from './FigmaComponent.module.scss';
+
+const FigmaComponent = () => {
+  return (
+    <div className={styles.frame7}>
+      <div className={styles.frame3}>
+        <div className={styles.frame1312333370}>
+          <div className={styles.ellipse1715} />
+          <div className={styles.autoWrapper}>
+            <div className={styles.frame2036084546}>
+              <p className={styles.text}>唐薇今日定榜榜值能否突破100万?</p>
+              <p className={styles.text2}>猜对即可瓜分总奖池</p>
+            </div>
+            <div className={styles.frame2036084617}>
+              <div className={styles.frame2036084557}>
+                <div className={styles.rectangle111142613}>
+                  <div className={styles.frame2036084556}>
+                    <p className={styles.text3}>能</p>
+                    <div className={styles.frame2036087387}>
+                      <p className={styles.text4}>回报</p>
+                      <div className={styles.frame}>
+                        <div className={styles.vectorStroke} />
+                        <div className={styles.vectorStroke2} />
+                      </div>
+                      <p className={styles.a119}>1.19</p>
+                      <p className={styles.text4}>倍</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.instance}>
+                <img
+                  src="./.figma/image/mev0klm6-w6xu5za.png"
+                  className={styles.frame2}
+                  alt="VS"
+                />
+              </div>
+              <div className={styles.frame2036084558}>
+                <img
+                  src="./.figma/image/mev0klm6-qwnqrkf.png"
+                  className={styles.rectangle1111426132}
+                  alt="Background"
+                />
+                <div className={styles.frame20360845562}>
+                  <p className={styles.text3}>不能</p>
+                  <div className={styles.frame2036087387}>
+                    <p className={styles.text4}>回报</p>
+                    <div className={styles.frame}>
+                      <div className={styles.vectorStroke} />
+                      <div className={styles.vectorStroke2} />
+                    </div>
+                    <p className={styles.a119}>6.22</p>
+                    <p className={styles.text4}>倍</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frame6}>
+        <div className={styles.frame13123333702}>
+          <div className={styles.ellipse17152} />
+          <div className={styles.frame2036084548}>
+            <div className={styles.frame1312333359}>
+              <div className={styles.frame1312333357}>
+                <p className={styles.text5}>热评</p>
+                <div className={styles.frame2036084610}>
+                  <div className={styles.frame4}>
+                    <div className={styles.avatar88} />
+                  </div>
+                  <div className={styles.frame2036084554}>
+                    <p className={styles.text6}>用户昵称：</p>
+                    <p className={styles.text2}>你看看我没错…</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.frame1312333355}>
+                <div className={styles.frame2036084581}>
+                  <img
+                    src="./.figma/image/mev0klm6-ik7b8h1.svg"
+                    className={styles.vector}
+                    alt="Like"
+                  />
+                  <p className={styles.a426}>426</p>
+                </div>
+                <div className={styles.frame2036084609}>
+                  <p className={styles.text6}>参与讨论</p>
+                  <img
+                    src="./.figma/image/mev0klm6-rj0j7s8.svg"
+                    className={styles.frame5}
+                    alt="Arrow"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FigmaComponent;
