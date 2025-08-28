5e9737f4c41a9da527e6a81b3f2559119995612f
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 12:04:53 +0800

    solo-5

---
+++ src/components/9_29591/index.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+import frameIcon from "../../image/meuvkrju-pbfsc0x.svg";
+import arrowIcon from "../../image/meuvkrju-6xcbmvu.svg";
+
+const Component = () => {
+  return (
+    <div className={styles.frame11}>
+      <div className={styles.autoWrapper2}>
+        <div className={styles.frame2}>
+          <div className={styles.autoWrapper}>
+            <div className={styles.frame1912054858}>
+              <img src={frameIcon} className={styles.frame} alt="" />
+            </div>
+            <p className={styles.text3}>
+              <span className={styles.text}>奈雪的茶(和盈中心</span>
+              <span className={styles.text2}>PRO</span>
+              <span className={styles.text}>店)等多店</span>
+            </p>
+          </div>
+          <img src={arrowIcon} className={styles.arrow} alt="" />
+        </div>
+        <p className={styles.text4}>待使用</p>
+      </div>
+      <div className={styles.frame6}>
+        <div className={styles.cover}>
+          <div className={styles.frame3}>
+            <div className={styles.mask} />
+          </div>
+        </div>
+        <div className={styles.frame4}>
+          <p className={styles.text7}>
+            <span className={styles.text5}>原叶鲜奶茶多选</span>
+            <span className={styles.text6}>1</span>
+          </p>
+          <p className={styles.text10}>
+            <span className={styles.text8}>有效期至：</span>
+            <span className={styles.text9}>2023-12-31</span>
+          </p>
+          <div className={styles.frame2036084632}>
+            <div className={styles.frame2036084604}>
+              <p className={styles.text11}>今日不可用</p>
+            </div>
+            <div className={styles.frame2036084604}>
+              <p className={styles.text11}>需预约</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frame5}>
+          <p className={styles.text14}>
+            <span className={styles.text12}>¥</span>
+            <span className={styles.text13}>&nbsp;</span>
+            <span className={styles.text6}>178.98</span>
+          </p>
+          <p className={styles.text10}>
+            <span className={styles.text8}>×</span>
+            <span className={styles.text9}>1</span>
+          </p>
+        </div>
+      </div>
+      <div className={styles.frame7}>
+        <p className={styles.text19}>
+          <span className={styles.text15}>请提醒商家使用</span>
+          <span className={styles.text16}>【</span>
+          <span className={styles.text17}>门店</span>
+          <span className={styles.text18}>POS</span>
+          <span className={styles.text17}>机</span>
+          <span className={styles.text16}>】</span>
+          <span className={styles.text15}>核销</span>
+        </p>
+      </div>
+      <div className={styles.frame8}>
+        <p className={styles.text20}>实付</p>
+        <p className={styles.text24}>
+          <span className={styles.text21}>¥</span>
+          <span className={styles.text22}>&nbsp;</span>
+          <span className={styles.text23}>178</span>
+          <span className={styles.text21}>.98</span>
+        </p>
+      </div>
+      <div className={styles.frame10}>
+        <div className={styles.instance}>
+          <p className={styles.text25}>申请退款</p>
+        </div>
+        <div className={styles.instance}>
+          <p className={styles.text25}>再来一单</p>
+        </div>
+        <div className={styles.frame9}>
+          <p className={styles.text26}>去使用</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
