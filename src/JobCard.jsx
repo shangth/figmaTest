57ce59c6d5c90b1568ae9b255caf9d575b54c19d
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 14:03:09 +0800

    solo-7

---
+++ src/JobCard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import styles from './JobCard.module.scss';
+
+const JobCard = () => {
+  return (
+    <div className={styles.frame}>
+      <div className={styles.wechatImg9121}>
+        <div className={styles.rectangle111141713}>
+          <p className={styles.text}>电子厂急招普工保安</p>
+          <p className={styles.text2}>招保安</p>
+        </div>
+      </div>
+      <div className={styles.rectangle111141715} />
+      <div className={styles.frame2036083627}>
+        <div className={styles.frame2036083617}>
+          <p className={styles.a30005000}>3000-5000</p>
+          <p className={styles.text3}>元/月</p>
+        </div>
+        <div className={styles.frame2036083514}>
+          <p className={styles.text4}>
+            岗位描述
+            <br />
+            1、工作内容是安装各种水电器零件
+            <br />
+            2、企业直招/无中介费押金
+          </p>
+          <p className={styles.text4}>
+            福利待遇
+            <br />
+            1、工作内容是安装各种水电器零件
+            <br />
+            2、企业直招/无中介费押金
+          </p>
+          <p className={styles.text4}>
+            工作地址
+            <br />
+            北京·昌平区，北京·大兴区
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default JobCard;
