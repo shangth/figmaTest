1cfdaedfec362a0a5edc381e7f5a84acafbf4cd8
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 20:08:23 +0800

    solo-14

---
+++ .figma/9_13375/index.jsx
@@ -0,0 +1,294 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.a39}>
+      <div className={styles.frame4}>
+        <img src="../image/mevc4lgm-z3kaqxg.svg" className={styles.statusBar} />
+        <div className={styles.frame3}>
+          <img src="../image/mevc4lgm-y9ctid2.svg" className={styles.frame} />
+          <div className={styles.frame2}>
+            <div className={styles.instance}>
+              <p className={styles.text}>商城</p>
+              <div className={styles.tabLine} />
+            </div>
+            <p className={styles.text2}>关注</p>
+            <p className={styles.text2}>发现</p>
+            <p className={styles.text2}>同城</p>
+            <p className={styles.text2}>直播</p>
+          </div>
+          <img src="../image/mevc4lgm-lr92wj4.svg" className={styles.frame} />
+        </div>
+      </div>
+      <div className={styles.autoWrapper4}>
+        <div className={styles.frame6}>
+          <div className={styles.frame5}>
+            <div className={styles.frame1312333005}>
+              <img src="../image/mevc4lgm-cbqph9r.svg" className={styles.icon} />
+              <p className={styles.text3}>女士外套冬季</p>
+            </div>
+            <div className={styles.frame1312333006}>
+              <p className={styles.text4}>搜索</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frame2036083534}>
+          <div className={styles.autoWrapper3}>
+            <div className={styles.autoWrapper}>
+              <div className={styles.group2036090922}>
+                <img
+                  src="../image/mevc4lgm-wcaf4ib.svg"
+                  className={styles.frame7}
+                />
+                <p className={styles.text5}>我的订单</p>
+              </div>
+              <div className={styles.frame2036083541}>
+                <img
+                  src="../image/mevc4lgm-vo0qba0.svg"
+                  className={styles.frame7}
+                />
+                <p className={styles.text5}>黑钻买家</p>
+              </div>
+              <div className={styles.frame2036083543}>
+                <img
+                  src="../image/mevc4lgm-lruumnd.svg"
+                  className={styles.frame7}
+                />
+                <p className={styles.text5}>购物车</p>
+              </div>
+            </div>
+            <div className={styles.autoWrapper2}>
+              <div className={styles.frame2036083542}>
+                <div className={styles.frame8}>
+                  <img
+                    src="../image/mevc4lgm-asp5gg0.svg"
+                    className={styles.fengkuangkanjia1}
+                  />
+                </div>
+                <p className={styles.text5}>砍价购</p>
+              </div>
+              <div className={styles.group2036090934}>
+                <div className={styles.group2036090972}>
+                  <div className={styles.group2036090965}>
+                    <p className={styles.text6}>爆</p>
+                  </div>
+                </div>
+                <p className={styles.text5}>直播爆款</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame9}>
+            <div className={styles.rectangle34624804}>
+              <div className={styles.rectangle34624803} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rectangle34624806}>
+        <div className={styles.autoWrapper5}>
+          <img src="../image/mevc4lgm-m4q7yrm.svg" className={styles.frame7} />
+          <p className={styles.text7}>大牌大补</p>
+          <p className={styles.text8}>正品保障</p>
+        </div>
+        <div className={styles.group2036090921}>
+          <div className={styles.frame2036083516}>
+            <img
+              src="../image/mevc4lgm-5jqwzx7.png"
+              className={styles.a6210Abcaa9F06B16952}
+            />
+            <div className={styles.frame1912054262}>
+              <p className={styles.text9}>砍价抄底</p>
+            </div>
+          </div>
+          <div className={styles.group1312332843}>
+            <p className={styles.a}>¥</p>
+            <p className={styles.text10}>224</p>
+          </div>
+        </div>
+        <div className={styles.group2036090920}>
+          <div className={styles.frame2036083517}>
+            <img
+              src="../image/mevc4lgm-ejv67xi.png"
+              className={styles.rectangle34624781}
+            />
+            <div className={styles.frame1912054263}>
+              <p className={styles.text9}>已补2.5元</p>
+            </div>
+          </div>
+          <div className={styles.group1312332844}>
+            <p className={styles.a}>¥</p>
+            <p className={styles.text10}>54</p>
+            <p className={styles.text11}>.</p>
+            <p className={styles.text12}>3</p>
+          </div>
+        </div>
+        <div className={styles.group2036090919}>
+          <div className={styles.frame2036083518}>
+            <img
+              src="../image/mevc4lgm-b2d0kju.png"
+              className={styles.abbb535Ccbbaf1581259}
+            />
+            <div className={styles.frame1912054264}>
+              <p className={styles.text9}>已补5元</p>
+            </div>
+          </div>
+          <div className={styles.group1312332845}>
+            <p className={styles.a}>¥</p>
+            <p className={styles.text10}>108</p>
+          </div>
+        </div>
+        <div className={styles.group2036090918}>
+          <div className={styles.frame2036083519}>
+            <img src="../image/mevc4lgm-knm0b76.png" className={styles.image1} />
+            <div className={styles.frame1912054265}>
+              <p className={styles.text9}>已补288元</p>
+            </div>
+          </div>
+          <div className={styles.group1312332842}>
+            <p className={styles.a}>¥</p>
+            <p className={styles.text10}>999</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.autoWrapper6}>
+        <div className={styles.feed}>
+          <div className={styles.a1}>
+            <div className={styles.frame14}>
+              <div className={styles.rectangle34624767}>
+                <div className={styles.frame1912054243}>
+                  <img
+                    src="../image/mevc4lgm-ru4owep.svg"
+                    className={styles.frame10}
+                  />
+                  <p className={styles.text13}>讲解回放</p>
+                </div>
+              </div>
+              <div className={styles.frame1312332868}>
+                <div className={styles.frame1312332862}>
+                  <div className={styles.frame1312334081}>
+                    <img
+                      src="../image/mevc4lgm-vsv5g76.png"
+                      className={styles.frame11}
+                    />
+                    <img
+                      src="../image/mevc4lgm-8wxwi6z.svg"
+                      className={styles.frame12}
+                    />
+                  </div>
+                  <p className={styles.text16}>
+                    <span className={styles.text14}>咖色咖啡杯</span>
+                    <span className={styles.text15}>【</span>
+                    <span className={styles.text14}>咖</span>
+                  </p>
+                </div>
+                <div className={styles.frame1312332841}>
+                  <p className={styles.text17}>满9.9减2</p>
+                  <p className={styles.text17}>好友买过</p>
+                  <p className={styles.text18}>破损包退</p>
+                </div>
+                <div className={styles.frame1312332833}>
+                  <div className={styles.frame13}>
+                    <p className={styles.a2}>￥</p>
+                    <div className={styles.frame1312320608}>
+                      <div className={styles.frame1312320799}>
+                        <p className={styles.text19}>128</p>
+                      </div>
+                      <p className={styles.text20}>.</p>
+                      <p className={styles.text21}>88</p>
+                    </div>
+                  </div>
+                  <p className={styles.text22}>已售1.5万+件</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.frame15}>
+              <img
+                src="../image/mevc4lgm-pr9r7q8.png"
+                className={styles.rectangle346247672}
+              />
+              <div className={styles.frame13123328622}>
+                <p className={styles.text23}>
+                  <span className={styles.text14}>咖色咖啡杯</span>
+                  <span className={styles.text15}>【</span>
+                  <span className={styles.text14}>咖</span>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.a22}>
+            <div className={styles.frame18}>
+              <img
+                src="../image/mevc4lgm-nb40iup.png"
+                className={styles.rectangle346247672}
+              />
+              <div className={styles.frame1312332869}>
+                <div className={styles.frame13123328623}>
+                  <div className={styles.rectangle6677}>
+                    <p className={styles.text24}>旗舰</p>
+                  </div>
+                  <p className={styles.text25}>格子短袖夏日款蓝色百</p>
+                </div>
+                <div className={styles.frame1312333207}>
+                  <div className={styles.frame13123328412}>
+                    <div className={styles.frame1312332837}>
+                      <div className={styles.group1312332836}>
+                        <div className={styles.rectangle34624791} />
+                        <div className={styles.rectangle34624792} />
+                        <div className={styles.rectangle34624793} />
+                      </div>
+                      <p className={styles.text17}>直播同价</p>
+                    </div>
+                    <div className={styles.frame1312332838}>
+                      <img
+                        src="../image/mevc4lgm-xxp3wsi.svg"
+                        className={styles.frame16}
+                      />
+                      <p className={styles.text17}>官方推荐</p>
+                    </div>
+                    <p className={styles.text26}>先用后付</p>
+                  </div>
+                  <div className={styles.frame13123328332}>
+                    <div className={styles.frame17}>
+                      <p className={styles.a2}>￥</p>
+                      <div className={styles.frame13123206082}>
+                        <div className={styles.frame13123207992}>
+                          <p className={styles.text19}>86</p>
+                        </div>
+                        <p className={styles.text20}>.</p>
+                        <p className={styles.text21}>8</p>
+                      </div>
+                    </div>
+                    <p className={styles.text22}>已售372件</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.frame19}>
+              <img
+                src="../image/mevc4lgm-91m27xb.png"
+                className={styles.rectangle346247672}
+              />
+              <div className={styles.frame13123328682} />
+            </div>
+          </div>
+        </div>
+        <div className={styles.frame22}>
+          <div className={styles.uIBarsHomeIndicatorH}>
+            <div className={styles.rectangle} />
+          </div>
+          <div className={styles.frame21}>
+            <p className={styles.text27}>首页</p>
+            <p className={styles.text28}>精选</p>
+            <img src="../image/mevc4lgm-8a4x2gl.svg" className={styles.frame20} />
+            <p className={styles.text29}>消息</p>
+            <p className={styles.text30}>我</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
