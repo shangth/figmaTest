6a7cc1a80c535e224d83d3a523cc9d2623d94ca7
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 20:36:58 +0800

    solo-17

---
+++ .figma/9_13658/index.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.frame16}>
+      <div className={styles.autoWrapper6}>
+        <div className={styles.ellipse3151} />
+        <div className={styles.group2036091757}>
+          <div className={styles.rectangle265496062}>
+            <div className={styles.autoWrapper4}>
+              <div className={styles.autoWrapper3}>
+                <div className={styles.frame} />
+                <div className={styles.autoWrapper2}>
+                  <div className={styles.autoWrapper}>
+                    <p className={styles.text}>美羊羊</p>
+                    <div className={styles.rectangle111141710}>
+                      <img
+                        src="../image/mevdq91h-qo45c9s.svg"
+                        className={styles.frame1912054208}
+                      />
+                      <img
+                        src="../image/mevdq91h-vz5pwfp.svg"
+                        className={styles.frame2036091808}
+                      />
+                    </div>
+                  </div>
+                  <p className={styles.text2}>特权有效期至：2024.08.31</p>
+                </div>
+              </div>
+              <div className={styles.group2036091692}>
+                <img
+                  src="../image/mevdq91h-0hbso86.svg"
+                  className={styles.frame2}
+                />
+                <p className={styles.text3}>特权生效中·服务您的购物之旅</p>
+              </div>
+            </div>
+            <img src="../image/mevdq91h-4vkhntz.svg" className={styles.maskGroup} />
+          </div>
+        </div>
+        <img src="../image/mevdq91h-rhf5rt2.svg" className={styles.statusBar} />
+        <div className={styles.frame2036091821}>
+          <div className={styles.frame4}>
+            <img src="../image/mevdq91h-rwh99vw.svg" className={styles.frame3} />
+          </div>
+          <img
+            src="../image/mevdq91h-uj4bv09.svg"
+            className={styles.frame2036091820}
+          />
+          <div className={styles.group2036091754}>
+            <img src="../image/mevdq91h-6n1u9hz.svg" className={styles.frame3} />
+            <p className={styles.text4}>客服</p>
+          </div>
+          <div className={styles.group20360917542}>
+            <div className={styles.frame6}>
+              <img src="../image/mevdq91h-wnjzadc.svg" className={styles.frame5} />
+            </div>
+            <p className={styles.text4}>规则</p>
+          </div>
+        </div>
+        <div className={styles.rectangle265496064}>
+          <p className={styles.text5}>尊享5大黑钻特权</p>
+        </div>
+        <div className={styles.frame9}>
+          <div className={styles.rectangle279334384}>
+            <img src="../image/mevdq91h-hdkmz65.svg" className={styles.frame7} />
+          </div>
+          <div className={styles.group2036091667}>
+            <div className={styles.autoWrapper5}>
+              <p className={styles.text}>退货包运费</p>
+              <div className={styles.frame2036085012}>
+                <p className={styles.text6}>本月剩余1次</p>
+              </div>
+            </div>
+            <p className={styles.text7}>每月6次，上门取件、到站寄使用均可使用</p>
+          </div>
+          <img src="../image/mevdq91h-ivgrshx.svg" className={styles.frame8} />
+        </div>
+      </div>
+      <div className={styles.frame11}>
+        <div className={styles.ellipse1}>
+          <img src="../image/mevdq91h-nm7mhqn.svg" className={styles.frame7} />
+        </div>
+        <div className={styles.group20360916672}>
+          <p className={styles.text}>极速退款</p>
+          <p className={styles.text7}>每月不限次数享受极速退款权益</p>
+        </div>
+        <img src="../image/mevdq91h-ib824mg.svg" className={styles.frame10} />
+      </div>
+      <div className={styles.frame13}>
+        <div className={styles.rectangle279334384}>
+          <img src="../image/mevdq91h-t81y24r.svg" className={styles.frame7} />
+        </div>
+        <div className={styles.group20360916672}>
+          <p className={styles.text}>专属客服</p>
+          <p className={styles.text7}>优先解答疑问，1v1全程跟进快速处理</p>
+        </div>
+        <img src="../image/mevdq91h-z26ax8e.svg" className={styles.frame12} />
+      </div>
+      <div className={styles.frame15}>
+        <div className={styles.rectangle279334384}>
+          <img src="../image/mevdq91h-coykdri.svg" className={styles.frame7} />
+        </div>
+        <div className={styles.group20360916672}>
+          <p className={styles.text}>优质取件</p>
+          <p className={styles.text7}>平台补贴，退货取件服务升级</p>
+        </div>
+        <img src="../image/mevdq91h-ecxrzqj.svg" className={styles.frame14} />
+      </div>
+      <div className={styles.frame15}>
+        <div className={styles.rectangle279334384}>
+          <img src="../image/mevdq91h-jqhi4td.svg" className={styles.frame7} />
+        </div>
+        <div className={styles.group20360916672}>
+          <p className={styles.text}>专属勋章</p>
+          <p className={styles.text7}>自动开启，彰显您的尊贵身份</p>
+        </div>
+        <img src="../image/mevdq91h-k3ppj4x.svg" className={styles.frame14} />
+      </div>
+      <img src="../image/mevdq91h-lrz566o.svg" className={styles.frame2036091840} />
+      <div className={styles.instance}>
+        <div className={styles.homeIndicator} />
+      </div>
+    </div>
+  );
+}
+
+export default Component;
