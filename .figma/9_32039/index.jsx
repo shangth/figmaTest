9f2ede2747ce64f09fd0c8411411c93c9137b000
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 11:42:21 +0800

    solo-3

---
+++ .figma/9_32039/index.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.left}>
+      <div className={styles.frame}>
+        <div className={styles.autoLayoutVertical}>
+          <img src="../image/meuuqg04-7qp4rue.svg" className={styles.icon} />
+          <p className={styles.transcript}>Trim</p>
+        </div>
+        <div className={styles.autoLayoutVertical2}>
+          <div className={styles.rectangle} />
+          <img src="../image/meuuqg04-hilq3mv.svg" className={styles.icon2} />
+          <p className={styles.media}>Captions</p>
+        </div>
+        <div className={styles.autoLayoutVertical}>
+          <img src="../image/meuuqg04-emzkb0l.svg" className={styles.icon} />
+          <p className={styles.transcript}>Music</p>
+        </div>
+      </div>
+      <div className={styles.frame3}>
+        <div className={styles.title}>
+          <div className={styles.autoLayoutHorizontal2}>
+            <div className={styles.autoLayoutHorizontal}>
+              <p className={styles.captions}>Captions</p>
+              <div className={styles.rectangle2} />
+            </div>
+            <p className={styles.captions2}>Preset</p>
+            <p className={styles.captions3}>Custom</p>
+          </div>
+          <div className={styles.icon3}>
+            <img src="../image/meuuqg04-llwn5ot.svg" className={styles.frame2} />
+          </div>
+        </div>
+        <div className={styles.chat}>
+          <div className={styles.autoWrapper2}>
+            <div className={styles.autoWrapper}>
+              <div className={styles.item}>
+                <div className={styles.intro}>
+                  <p className={styles.speaker1}>Speaker 1</p>
+                  <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+                </div>
+                <div className={styles.text}>
+                  <p className={styles.the}>Hey,</p>
+                  <p className={styles.the}>nice</p>
+                  <p className={styles.the}>to</p>
+                  <p className={styles.the}>meet</p>
+                  <p className={styles.the}>you.</p>
+                </div>
+              </div>
+              <div className={styles.item2}>
+                <div className={styles.intro}>
+                  <p className={styles.speaker1}>Speaker 1</p>
+                  <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+                </div>
+                <div className={styles.text}>
+                  <p className={styles.the}>My</p>
+                  <p className={styles.the}>name</p>
+                  <p className={styles.the}>is</p>
+                  <p className={styles.the}>David.</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.rectangle279334408} />
+          </div>
+          <div className={styles.item3}>
+            <div className={styles.intro2}>
+              <p className={styles.speaker12}>Speaker 2</p>
+              <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+            </div>
+            <div className={styles.text2}>
+              <p className={styles.the2}>I’m</p>
+              <p className={styles.the2}>the</p>
+              <p className={styles.the2}>co-founder</p>
+              <p className={styles.the2}>of</p>
+              <p className={styles.the2}>CyberCut.</p>
+            </div>
+          </div>
+          <div className={styles.item4}>
+            <div className={styles.intro2}>
+              <p className={styles.speaker12}>Speaker 2</p>
+              <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+            </div>
+            <div className={styles.text3}>
+              <p className={styles.the3}>Welcome</p>
+              <p className={styles.the3}>on</p>
+              <p className={styles.the3}>the</p>
+              <p className={styles.the3}>product.</p>
+            </div>
+          </div>
+          <div className={styles.item5}>
+            <div className={styles.intro}>
+              <p className={styles.speaker1}>Speaker 1</p>
+              <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+            </div>
+            <div className={styles.text}>
+              <p className={styles.the}>So</p>
+              <p className={styles.the}>if</p>
+              <p className={styles.the}>you</p>
+              <p className={styles.the}>are</p>
+              <p className={styles.the}>watching</p>
+              <p className={styles.the}>this</p>
+              <p className={styles.the}>video</p>
+              <p className={styles.the}>is</p>
+              <p className={styles.the}>probably</p>
+              <p className={styles.the}>that</p>
+              <p className={styles.the}>you</p>
+              <p className={styles.the}>are.</p>
+            </div>
+          </div>
+          <div className={styles.item5}>
+            <div className={styles.intro}>
+              <p className={styles.speaker1}>Speaker 1</p>
+              <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+            </div>
+            <div className={styles.text}>
+              <p className={styles.the}>What</p>
+              <p className={styles.the}>kind</p>
+              <p className={styles.the}>of</p>
+              <p className={styles.the}>style</p>
+              <p className={styles.the}>of</p>
+              <p className={styles.the}>templates</p>
+              <p className={styles.the}>and</p>
+              <p className={styles.the}>style.</p>
+            </div>
+          </div>
+          <div className={styles.item5}>
+            <div className={styles.intro}>
+              <p className={styles.speaker1}>Speaker 1</p>
+              <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+            </div>
+            <div className={styles.text}>
+              <p className={styles.the}>So</p>
+              <p className={styles.the}>a</p>
+              <p className={styles.the}>good</p>
+              <p className={styles.the}>test</p>
+              <p className={styles.the}>it.</p>
+            </div>
+          </div>
+          <div className={styles.item5}>
+            <div className={styles.intro}>
+              <p className={styles.speaker1}>Speaker 1</p>
+              <p className={styles.a120000120024}>12:00:00-12:00:24</p>
+            </div>
+            <div className={styles.text}>
+              <p className={styles.the}>Go</p>
+              <p className={styles.the}>on</p>
+              <p className={styles.the}>the</p>
+              <p className={styles.the}>section</p>
+              <p className={styles.the}>templates</p>
+              <p className={styles.the}>and</p>
+              <p className={styles.the}>style.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,160 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="left">
+      <div className="frame">
+        <div className="autoLayoutVertical">
+          <img src="./.figma/image/meuuqg04-7qp4rue.svg" className="icon" alt="Trim" />
+          <p className="transcript">Trim</p>
+        </div>
+        <div className="autoLayoutVertical2">
+          <div className="rectangle" />
+          <img src="./.figma/image/meuuqg04-hilq3mv.svg" className="icon2" alt="Captions" />
+          <p className="media">Captions</p>
+        </div>
+        <div className="autoLayoutVertical">
+          <img src="./.figma/image/meuuqg04-emzkb0l.svg" className="icon" alt="Music" />
+          <p className="transcript">Music</p>
+        </div>
+      </div>
+      <div className="frame3">
+        <div className="title">
+          <div className="autoLayoutHorizontal2">
+            <div className="autoLayoutHorizontal">
+              <p className="captions">Captions</p>
+              <div className="rectangle2" />
+            </div>
+            <p className="captions2">Preset</p>
+            <p className="captions3">Custom</p>
+          </div>
+          <div className="icon3">
+            <img src="./.figma/image/meuuqg04-llwn5ot.svg" className="frame2" alt="Settings" />
+          </div>
+        </div>
+        <div className="chat">
+          <div className="autoWrapper2">
+            <div className="autoWrapper">
+              <div className="item">
+                <div className="intro">
+                  <p className="speaker1">Speaker 1</p>
+                  <p className="a120000120024">12:00:00-12:00:24</p>
+                </div>
+                <div className="text">
+                  <p className="the">Hey,</p>
+                  <p className="the">nice</p>
+                  <p className="the">to</p>
+                  <p className="the">meet</p>
+                  <p className="the">you.</p>
+                </div>
+              </div>
+              <div className="item2">
+                <div className="intro">
+                  <p className="speaker1">Speaker 1</p>
+                  <p className="a120000120024">12:00:00-12:00:24</p>
+                </div>
+                <div className="text">
+                  <p className="the">My</p>
+                  <p className="the">name</p>
+                  <p className="the">is</p>
+                  <p className="the">David.</p>
+                </div>
+              </div>
+            </div>
+            <div className="rectangle279334408" />
+          </div>
+          <div className="item3">
+            <div className="intro2">
+              <p className="speaker12">Speaker 2</p>
+              <p className="a120000120024">12:00:00-12:00:24</p>
+            </div>
+            <div className="text2">
+              <p className="the2">I'm</p>
+              <p className="the2">the</p>
+              <p className="the2">co-founder</p>
+              <p className="the2">of</p>
+              <p className="the2">CyberCut.</p>
+            </div>
+          </div>
+          <div className="item4">
+            <div className="intro2">
+              <p className="speaker12">Speaker 2</p>
+              <p className="a120000120024">12:00:00-12:00:24</p>
+            </div>
+            <div className="text3">
+              <p className="the3">Welcome</p>
+              <p className="the3">on</p>
+              <p className="the3">the</p>
+              <p className="the3">product.</p>
+            </div>
+          </div>
+          <div className="item5">
+            <div className="intro">
+              <p className="speaker1">Speaker 1</p>
+              <p className="a120000120024">12:00:00-12:00:24</p>
+            </div>
+            <div className="text">
+              <p className="the">So</p>
+              <p className="the">if</p>
+              <p className="the">you</p>
+              <p className="the">are</p>
+              <p className="the">watching</p>
+              <p className="the">this</p>
+              <p className="the">video</p>
+              <p className="the">is</p>
+              <p className="the">probably</p>
+              <p className="the">that</p>
+              <p className="the">you</p>
+              <p className="the">are.</p>
+            </div>
+          </div>
+          <div className="item5">
+            <div className="intro">
+              <p className="speaker1">Speaker 1</p>
+              <p className="a120000120024">12:00:00-12:00:24</p>
+            </div>
+            <div className="text">
+              <p className="the">What</p>
+              <p className="the">kind</p>
+              <p className="the">of</p>
+              <p className="the">style</p>
+              <p className="the">of</p>
+              <p className="the">templates</p>
+              <p className="the">and</p>
+              <p className="the">style.</p>
+            </div>
+          </div>
+          <div className="item5">
+            <div className="intro">
+              <p className="speaker1">Speaker 1</p>
+              <p className="a120000120024">12:00:00-12:00:24</p>
+            </div>
+            <div className="text">
+              <p className="the">So</p>
+              <p className="the">a</p>
+              <p className="the">good</p>
+              <p className="the">test</p>
+              <p className="the">it.</p>
+            </div>
+          </div>
+          <div className="item5">
+            <div className="intro">
+              <p className="speaker1">Speaker 1</p>
+              <p className="a120000120024">12:00:00-12:00:24</p>
+            </div>
+            <div className="text">
+              <p className="the">Go</p>
+              <p className="the">on</p>
+              <p className="the">the</p>
+              <p className="the">section</p>
+              <p className="the">templates</p>
+              <p className="the">and</p>
+              <p className="the">style.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
