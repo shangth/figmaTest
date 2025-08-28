e551ee477a283baf429cb935fc2ce765a9e29b56
shangtianhan <shangtianhan@kuaishou.com>
2025-08-28 20:14:55 +0800

    solo-15

---
+++ .figma/9_1829/index.jsx
@@ -0,0 +1,435 @@
+import React from 'react';
+
+import styles from './index.module.scss';
+
+const Component = () => {
+  return (
+    <div className={styles.frame39}>
+      <div className={styles.instance}>
+        <div className={styles.frame}>
+          <div className={styles.a92A5465945A447C2Da6} />
+        </div>
+      </div>
+      <div className={styles.frame23}>
+        <div className={styles.frame2036095795}>
+          <div className={styles.a222}>
+            <div className={styles.aIcon2}>
+              <div className={styles.frame2}>
+                <div className={styles.vector} />
+                <div className={styles.vector4}>
+                  <div className={styles.vector3}>
+                    <div className={styles.vector2} />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.group2036088409}>
+                <img
+                  src="../image/mevd4ekg-49o8n0b.png"
+                  className={styles.frame3}
+                />
+                <img src="../image/mevd4ekg-5qvavha.png" className={styles.star2} />
+                <img src="../image/mevd4ekg-yx0fo0g.png" className={styles.star3} />
+              </div>
+              <div className={styles.vector5}>
+                <p className={styles.text}>集星规则</p>
+              </div>
+            </div>
+            <div className={styles.frame2036095790}>
+              <p className={styles.text2}>集星兑豪礼 钻石主播得额外流量</p>
+              <div className={styles.frame2036095785}>
+                <p className={styles.text3}>去签约</p>
+                <img
+                  src="../image/mevd4ekg-fem8r9u.svg"
+                  className={styles.frame4}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame13}>
+            <div className={styles.frame2036095762}>
+              <p className={styles.text4}>600</p>
+              <p className={styles.text5}>/100星领取</p>
+              <p className={styles.text5}>不消耗</p>
+              <div className={styles.autoWrapper}>
+                <div className={styles.image5731}>
+                  <img
+                    src="../image/mevd4ekg-7cujy04.png"
+                    className={styles.image5732}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={styles.frame2036095781}>
+              <div className={styles.instance2}>
+                <div className={styles.frame6}>
+                  <img
+                    src="../image/mevd4ekg-76u1y2d.png"
+                    className={styles.frame5}
+                  />
+                </div>
+                <p className={styles.text5}>名称名称名称</p>
+                <div className={styles.frame2036095709}>
+                  <div className={styles.frame2036095693}>
+                    <p className={styles.a400}>430</p>
+                    <img
+                      src="../image/mevd4ekg-qnmcz0t.png"
+                      className={styles.frame20360958401}
+                    />
+                  </div>
+                  <p className={styles.a400}>兑换</p>
+                </div>
+              </div>
+              <div className={styles.frame9}>
+                <div className={styles.frame2036095709}>
+                  <div className={styles.frame2036095693}>
+                    <p className={styles.a400}>255</p>
+                    <img
+                      src="../image/mevd4ekg-qnmcz0t.png"
+                      className={styles.frame20360958401}
+                    />
+                  </div>
+                  <p className={styles.a400}>兑换</p>
+                </div>
+                <div className={styles.frame8}>
+                  <img
+                    src="../image/mevd4ekg-1y758wx.png"
+                    className={styles.frame7}
+                  />
+                </div>
+                <p className={styles.text6}>名称名称名称</p>
+              </div>
+              <div className={styles.instance4}>
+                <div className={styles.instance3}>
+                  <img
+                    src="../image/mevd4ekg-pvujiqf.png"
+                    className={styles.image1201}
+                  />
+                </div>
+                <p className={styles.text5}>春季之星礼物</p>
+                <div className={styles.frame2036095763}>
+                  <div className={styles.frame20360956932}>
+                    <p className={styles.a40}>40</p>
+                    <div className={styles.image57312}>
+                      <img
+                        src="../image/mevd4ekg-jq991m0.png"
+                        className={styles.image57322}
+                      />
+                    </div>
+                  </div>
+                  <p className={styles.a40}>兑换</p>
+                </div>
+              </div>
+              <div className={styles.frame12}>
+                <div className={styles.frame11}>
+                  <img
+                    src="../image/mevd4ekg-2lv8nf1.png"
+                    className={styles.frame10}
+                  />
+                </div>
+                <p className={styles.text5}>1%加成</p>
+                <div className={styles.frame2036095763}>
+                  <div className={styles.frame20360956932}>
+                    <p className={styles.a40}>40</p>
+                    <div className={styles.image57312}>
+                      <img
+                        src="../image/mevd4ekg-jq991m0.png"
+                        className={styles.image57322}
+                      />
+                    </div>
+                  </div>
+                  <p className={styles.a40}>兑换</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame22}>
+            <div className={styles.instance8}>
+              <div className={styles.instance6}>
+                <div className={styles.frame15}>
+                  <div className={styles.frame14}>
+                    <p className={styles.text7}>开</p>
+                  </div>
+                </div>
+                <div className={styles.instance5}>
+                  <div className={styles.rectangle2}>
+                    <div className={styles.rectangle}>
+                      <img
+                        src="../image/mevd4ekg-p8qw5nh.png"
+                        className={styles.frame16}
+                      />
+                      <p className={styles.text8}>立即抢</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.instance7}>
+                <div className={styles.frame18}>
+                  <div className={styles.frame17}>
+                    <p className={styles.text7}>开</p>
+                  </div>
+                </div>
+                <div className={styles.instance5}>
+                  <div className={styles.rectangle2}>
+                    <div className={styles.rectangle}>
+                      <img
+                        src="../image/mevd4ekg-p8qw5nh.png"
+                        className={styles.frame16}
+                      />
+                      <p className={styles.text8}>立即抢</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.frame21}>
+              <div className={styles.frame20}>
+                <div className={styles.frame19}>
+                  <p className={styles.text9}>
+                    盛典
+                    <br />
+                    状元
+                  </p>
+                </div>
+              </div>
+              <div className={styles.instance9}>
+                <div className={styles.rectangle4}>
+                  <div className={styles.rectangle3}>
+                    <p className={styles.text10}>领复活卡&nbsp;</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frame20360957802}>
+        <div className={styles.instance12}>
+          <div className={styles.frame35}>
+            <div className={styles.miaobian2}>
+              <div className={styles.miaobian1}>
+                <div className={styles.frame2036095739}>
+                  <div className={styles.ellipse3294} />
+                  <div className={styles.frame2036095738}>
+                    <div className={styles.frame2036095771}>
+                      <p className={styles.text11}>累计集星：290</p>
+                      <img
+                        src="../image/mevd4ekg-y5x8wgx.svg"
+                        className={styles.frame4}
+                      />
+                    </div>
+                    <div className={styles.frame2036095740}>
+                      <p className={styles.text12}>当前拥有:</p>
+                      <div className={styles.frame2036095692}>
+                        <p className={styles.a150}>150</p>
+                        <img
+                          src="../image/mevd4ekg-hs74ilg.png"
+                          className={styles.frame203609584012}
+                        />
+                      </div>
+                      <div className={styles.frame20360956933}>
+                        <p className={styles.a140}>140</p>
+                        <img
+                          src="../image/mevd4ekg-kok9yr1.png"
+                          className={styles.frame20360958401}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.frame2036095742}>
+                  <div className={styles.instance10}>
+                    <div className={styles.vector8}>
+                      <div className={styles.vector7}>
+                        <div className={styles.vector6} />
+                      </div>
+                    </div>
+                    <img
+                      src="../image/mevd4ekg-oqljvr1.png"
+                      className={styles.frame24}
+                    />
+                    <div className={styles.frame28}>
+                      <div className={styles.frame26}>
+                        <div className={styles.frame25} />
+                      </div>
+                      <div className={styles.ellipse3263} />
+                      <div className={styles.ellipse3264} />
+                      <div className={styles.frame27}>
+                        <p className={styles.text13}>兑奖记录</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.instance11}>
+                    <div className={styles.group1312333214}>
+                      <div className={styles.vector9}>
+                        <div className={styles.vector7}>
+                          <div className={styles.vector6} />
+                        </div>
+                      </div>
+                      <div className={styles.frame30}>
+                        <img
+                          src="../image/mevd4ekg-nd8fft0.png"
+                          className={styles.frame29}
+                        />
+                      </div>
+                      <img
+                        src="../image/mevd4ekg-cwmze8k.png"
+                        className={styles.image653}
+                      />
+                      <img
+                        src="../image/mevd4ekg-7rp3w75.png"
+                        className={styles.image655}
+                      />
+                      <img
+                        src="../image/mevd4ekg-y9kwvuh.png"
+                        className={styles.image656}
+                      />
+                      <img
+                        src="../image/mevd4ekg-ib9twia.png"
+                        className={styles.image654}
+                      />
+                    </div>
+                    <div className={styles.frame34}>
+                      <div className={styles.frame32}>
+                        <div className={styles.frame31} />
+                      </div>
+                      <div className={styles.ellipse32632} />
+                      <div className={styles.ellipse32642} />
+                      <div className={styles.frame33}>
+                        <p className={styles.text13}>赛事荣誉</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frame2036095767}>
+          <div className={styles.frame2036095753}>
+            <div className={styles.instance13}>
+              <div className={styles.image5737}>
+                <p className={styles.text14}>45%</p>
+              </div>
+            </div>
+            <div className={styles.frame2036095752}>
+              <div className={styles.frame2036095751}>
+                <p className={styles.text17}>
+                  <span className={styles.text15}>累计</span>
+                  <span className={styles.text16}>2332</span>
+                  <span className={styles.text15}>/50000盛典值</span>
+                </p>
+                <div className={styles.frame2036094475}>
+                  <img
+                    src="../image/mevd4ekg-06o3mco.svg"
+                    className={styles.frame36}
+                  />
+                  <p className={styles.text18}>02:59</p>
+                </div>
+              </div>
+              <div className={styles.frame2036095759}>
+                <p className={styles.text19}>可获得&nbsp;</p>
+                <div className={styles.frame20360957812}>
+                  <img
+                    src="../image/mevd4ekg-9ika4vb.png"
+                    className={styles.frame203609584013}
+                  />
+                  <p className={styles.text20}>&nbsp;+ 5万流量</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame2036095757}>
+            <div className={styles.frame2036095755}>
+              <div className={styles.frame2036095754}>
+                <p className={styles.text21}>分享邀请码&nbsp;&nbsp;邀请好友组队</p>
+                <div className={styles.frame1312333259}>
+                  <p className={styles.kStz1036}>KSTZ1036</p>
+                  <div className={styles.frame37}>
+                    <img
+                      src="../image/mevd4ekg-k72a0vl.svg"
+                      className={styles.frame4}
+                    />
+                    <p className={styles.kStz1036}>复制</p>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.frame2036095780}>
+                <p className={styles.text22}>
+                  组队后每日双方各完成1次盛典值挑战额外得1
+                </p>
+                <img
+                  src="../image/mevd4ekg-7e657ts.png"
+                  className={styles.frame20360958401}
+                />
+              </div>
+            </div>
+            <div className={styles.frame38}>
+              <p className={styles.text23}>加入队伍</p>
+            </div>
+          </div>
+          <div className={styles.frame1312318723}>
+            <p className={styles.text20}>再挑战x轮可集</p>
+            <img
+              src="../image/mevd4ekg-vw41tbj.png"
+              className={styles.frame203609584013}
+            />
+          </div>
+        </div>
+        <div className={styles.frame2036095794}>
+          <div className={styles.frame20360957522}>
+            <p className={styles.text26}>
+              <span className={styles.text24}>再得</span>
+              <span className={styles.text25}>23332</span>
+              <span className={styles.text24}>评论</span>
+            </p>
+            <div className={styles.frame20360957592}>
+              <p className={styles.text19}>可获得&nbsp;</p>
+              <div className={styles.frame2036095770}>
+                <p className={styles.text20}>1</p>
+                <img
+                  src="../image/mevd4ekg-ajeywmn.png"
+                  className={styles.frame203609584013}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame20360957523}>
+            <p className={styles.text26}>
+              <span className={styles.text24}>再得</span>
+              <span className={styles.text25}>233332</span>
+              <span className={styles.text24}>点赞</span>
+            </p>
+            <div className={styles.frame20360957593}>
+              <p className={styles.text19}>可获得&nbsp;</p>
+              <div className={styles.frame2036095770}>
+                <p className={styles.text20}>1</p>
+                <img
+                  src="../image/mevd4ekh-rsxkurm.png"
+                  className={styles.frame203609584013}
+                />
+              </div>
+            </div>
+          </div>
+          <div className={styles.frame20360957524}>
+            <p className={styles.text26}>
+              <span className={styles.text24}>开播任务&nbsp;</span>
+              <span className={styles.text25}>挑战成功</span>
+            </p>
+            <div className={styles.frame20360957594}>
+              <p className={styles.text19}>可获得&nbsp;</p>
+              <div className={styles.frame2036095770}>
+                <p className={styles.text20}>1</p>
+                <img
+                  src="../image/mevd4ekh-4ddh0k8.png"
+                  className={styles.frame203609584013}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Component;
